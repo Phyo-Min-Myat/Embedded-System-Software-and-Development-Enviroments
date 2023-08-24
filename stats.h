ec0cee6c5512bfe63467d26bcacdafc93cf18f96
PhyoMinMyat <simonphyomin@gmail.com>
2023-08-24 12:05:42 +0630

    first commit

---
+++ stats.h
@@ -0,0 +1,38 @@
+/***********************************************************************************
+*Copyright (C) 2023 by Phyo Min Myat - Swan Arr Electronics and Industries (S.A.E.I)
+*
+*Redistribution, modification or use of this software in a source or binary forms is
+*permitted as long as the file maintain this copyright. Users are permitted to modify
+*this and use it to learn about the field of embedded software. Phyo Min Myat and the 
+*Swan Arr Electronics and Industries (S.A.E.I) are not liable for any misuse of this 
+*material.
+*
+************************************************************************************/
+/**
+ * @file <Add File Name>
+ * @brief<Add Brief Description Here>
+ * 
+ * <Add Extended Description Here>
+ * 
+ * @author <Add First LastName>
+ * @date <Add date>
+*/
+#ifndef __STATS_H__
+#define __STATS_H__
+
+/* Add Your Declarations and function Comments here */
+
+/**
+ * @brief  <Add Bried Description of Function Here>
+ * 
+ * <Add Extended Description Here>
+ * 
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * 
+ * @return <Add Return Information here>
+ */
+
+#endif /* __STATS_H__ */
